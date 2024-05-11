275ef729ef748a81891e489a856981e74fb25279
Jakub Lerka <jakublerka01@gmail.com>
2024-05-11 15:32:22 +0200

    Zapisywanie i odczytywanie z pliku

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public int[][] szachownica = new int[8][8];
+    static Scanner scanner = new Scanner(System.in);
+    //private String filePathUserInput;
+    public static String userInput;
+
+    //Funkcja ma zapisywać do pliku
+    public static void saveToFile() throws IOException {
+        System.out.println("Podaj ściężke do pliku do którego mają zostać zapisane dane:");
+        Path filePathUserInput = Path.of(scanner.nextLine());
+        System.out.println("Podaj współrzędne hetmana które chcesz zapisać do wskazanego pliku:");
+        userInput = scanner.nextLine();
+        Files.write(filePathUserInput, userInput.getBytes());
     }
+
+    //Funkcja ma odczytywać z pliku
+    public static void readFromFile() throws IOException {
+        System.out.println("Podaj ściężke do pliku z którego mają zostać odczytane dane:");
+        Path filePathUserInput = Path.of(scanner.nextLine());
+        System.out.println("Współrzędne hetmana zapisane w podanym pliku:");
+        Files.readAllLines(filePathUserInput);
+    }
+
+    //Funkcja ma wstawiać hetmana
+    public void wstawHetmana() {}
+
+    //Funkcja ma być tylko zadklerowana
+    public void count() {}
+
+    public static void main(String[] args) throws IOException {
+        saveToFile();
+        readFromFile();
+    }
+
+
+
 }
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+
+public class ExampleTest {
+
+}
