81288111448f3a4503c2e4dceddbc223375f0dff
Jakub Lerka <jakublerka01@gmail.com>
2024-05-22 20:47:34 +0200

    Przetestowano zapis i odczyt z pliku

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,34 +9,52 @@ public class Main {
     public int[][] szachownica = new int[8][8];
     static Scanner scanner = new Scanner(System.in);
     //private String filePathUserInput;
-    public static String userInput;
 
-    //Funkcja ma zapisywać do pliku
-    public static void saveToFile() throws IOException {
-        System.out.println("Podaj ściężke do pliku do którego mają zostać zapisane dane:");
-        Path filePathUserInput = Path.of(scanner.nextLine());
+    public static String userInput;
+    public static Path filePathUserInput;
+    public static String output;
+
+    public static void readPathToSave() {
+        System.out.println("Podaj ściężke do pliku do którego mają zostać zapisane/odczytane dane:");
+        filePathUserInput = Path.of(scanner.nextLine());
+    }
+
+    public static void inputToSaveToFile() {
         System.out.println("Podaj współrzędne hetmana które chcesz zapisać do wskazanego pliku:");
         userInput = scanner.nextLine();
+    }
+
+    //Funkcja ma zapisywać do pliku
+    public static void saveToFile(Path filePathUserInput, String userInput) throws IOException {
+        //System.out.println("Podaj ściężke do pliku do którego mają zostać zapisane dane:");
+        //filePathUserInput = Path.of(scanner.nextLine());
         Files.write(filePathUserInput, userInput.getBytes());
     }
 
     //Funkcja ma odczytywać z pliku
-    public static void readFromFile() throws IOException {
+    public static void readFromFile(Path filePathUserInput) throws IOException {
         System.out.println("Podaj ściężke do pliku z którego mają zostać odczytane dane:");
-        Path filePathUserInput = Path.of(scanner.nextLine());
+        //filePathUserInput = Path.of(scanner.nextLine());
         System.out.println("Współrzędne hetmana zapisane w podanym pliku:");
-        Files.readAllLines(filePathUserInput);
+        output = Files.readAllLines(filePathUserInput).toString();
+        System.out.println(output);
     }
 
     //Funkcja ma wstawiać hetmana
-    public void wstawHetmana() {}
+    public static void wstawHetmana() {}
 
     //Funkcja ma być tylko zadklerowana
-    public void count() {}
+    public static int count() {
+        return 0;
+    }
 
     public static void main(String[] args) throws IOException {
-        saveToFile();
-        readFromFile();
+        readPathToSave();
+        inputToSaveToFile();
+        saveToFile(filePathUserInput, userInput);
+        readPathToSave();
+        readFromFile(filePathUserInput);
+        count();
     }
 
 
--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,8 +1,64 @@
+import org.example.Main;
+import org.junit.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.testng.annotations.AfterTest;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class ExampleTest {
+    //Inicjalizacja plikow testowych i spodziewanego outputu
+    Path preppedFilePath;
+    String expectedOutput;
+    String input;
+
+    //Nadanie wartosci przed wykanoniem pierwszego testu
+    @Before
+    public void setup() {
+        preppedFilePath = Path.of("test.csv");
+        Files mockFile = Mockito.mock(Files.class);
+    }
+
+    //Czyszczenie zmiennych input, expectedOutput po kazdym tescie, tak aby upewnic sie ze na pewno dane zostaly usuniete
+    @AfterTest
+    public void teardown() {
+        expectedOutput = "";
+        input="";
+    }
+
+    // ---- WLASNE ROZWIAZANIE ----
+
+    /*
+    @Test
+    public void testWritingToFile() throws Exception {
+        input="2,0";
+        Main.saveToFile(preppedFilePath, input);
+    }*/
+
+
+    @Test
+    public void testReadingFromFile() throws Exception {
+        expectedOutput = "[2,0]";
+        Main.readFromFile(preppedFilePath);
+
+        //System.out.println("Dane znajdujące się w preparowanym pliku: "+ preppedOutput); //Ta linia nie ma sensu
+        assertEquals(Main.output, expectedOutput);
+    }
+
+    @Test
+    public void testWritingAndReading() throws Exception {
+        input="2,0";
+        Main.saveToFile(preppedFilePath, input);
+
+        expectedOutput="[2,0]";
+        Main.readFromFile(preppedFilePath);
+        assertEquals(Main.output, expectedOutput);
+    }
 
 }
